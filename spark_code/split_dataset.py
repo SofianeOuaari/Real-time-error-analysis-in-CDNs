d3c472866bc9c72bbc78448af78bbc1b93225f59
SofianeOuaari <sofiane.ouaari@gmail.com>
2021-11-23 23:00:27 +0000

    Code Cleaning

--- spark_code/split_dataset.py
+++ spark_code/split_dataset.py
@@ -6,7 +6,11 @@ import pandas as pd
 
 
 df=pd.read_csv("./data/CDN Client Error.csv",sep=",")
-df.set_index("")
+'''df=df[["timestamp","channel_id","host_id","content_type","protocol","content_id","geo_location","user_id"]]
+
+print(df.head())
+
+df.to_csv("./data/CDN Client Error.csv",index=False)'''
 '''df_train=df.iloc[:int(len(df)*0.75)]
 df_test=df.iloc[int(len(df)*0.75):]'''
 df_train_cdn=df.iloc[:50000]
