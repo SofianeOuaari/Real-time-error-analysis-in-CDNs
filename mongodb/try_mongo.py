3b60095762c8330d952cb0a2116493295438b9fd
hkristof03 <hkristof03@hotmail.com>
2021-11-24 17:46:08 +0100

    Restructure project repository

--- mongodb/try_mongo.py
+++
@@ -1,9 +0,0 @@
-import pymongo  # package for working with MongoDB
-client = pymongo.MongoClient("mongodb://localhost:27017/")
-db = client["prediction"]
-customers = db["prediction"]
-
-
-
-for x in customers.find():
-    print(x)
