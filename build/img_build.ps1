3a1701c8504daafb62c3234b372e76213254c5fb
Florian <florian.spire@etu.univ-cotedazur.fr>
2021-11-28 02:48:47 +0100

    Burrow building
    Burrow building to make it work under Windows

--- build/img_build.ps1
+++ build/img_build.ps1
@@ -2,6 +2,7 @@ Write-Host "Setting environment variables...";
 $curDir = Get-Location
 $Env:CDN_REGISTRY = "localhost:5000"
 $Env:CDN_HOME = Split-Path -Path $curDir -Parent
+$Env:BURROW_SRC = "D:\OneDrive - Université Nice Sophia Antipolis\M2\Stream Mining\Project\Real-time-error-analysis-in-CDNs\source\Burrow"
 Write-Host "Done";
 
 docker run -d -p 5000:5000 --restart=always --name registry registry:2
