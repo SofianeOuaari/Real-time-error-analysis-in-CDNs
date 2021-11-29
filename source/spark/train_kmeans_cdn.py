ff4435ff539b908078340f58afe1b820d55c8d26
Florian <florian.spire@etu.univ-cotedazur.fr>
2021-11-29 07:29:01 +0100

    HDBSCAN implementation (training and real-time clustering)
    Implementation of the HBSCAN algorithm. Adding of the algorithm in the spark scripts, training on the train dataset and predictions from the incoming data in real-time. 
    
    This commit also includes the notebook that I created to briefly present HDSBSCAN and to tune the parameters. The spark docker files (to build the image) have been updated to include the HDBSCAN libraries in the spark container.

--- source/spark/train_kmeans_cdn.py
+++ source/spark/train_kmeans_cdn.py
@@ -30,12 +30,14 @@ if __name__=="__main__":
     
     model_1.fit(df_train_ohe)
 
-    model_1_path_name="models/kmeans.pickle"
+    model_1_path_name="models/kmeans_7.pickle"
     encoder_path_name="processing_obj/ohe.pickle"
     
     dump(model_1,model_1_path_name)
     
     dump(encoder,encoder_path_name)
+    
+    
 
 
 
